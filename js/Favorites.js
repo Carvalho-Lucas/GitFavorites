9ce120e3efd4ce2d09a11f84846919128ebd4ef5
Lucão <lukaspc2019@gmail.com>
2023-04-27 22:31:40 -0300

    Atualização Classes do APP

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,32 +1,47 @@
-//Conexão com Api do git para salvar JSON em localStorage
-export class GitHubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`;
+import { GitHubUser } from "./GithubUser.js";
 
-    return fetch(endpoint)
-      .then((data) => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers,
-      }));
-  }
-}
-
-//classe para conter a lógica dos dados
+//Classe para conter a lógica dos dados
 //Como os dados serão estruturados
 
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.load();
+
+   
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username){
+
+   
+    try{
+      const userExists = this.entries.find(entry => entry.login === username)
+      console.log(userExists)
+
+      if(userExists){
+        throw new Error('Usuário já cadastrado!')
+      }
+        const user = await GitHubUser.search(username)
+
+        if(user.login === undefined){
+          throw new Error('Usuário não encontrado!')
+        }
+        this.entries = [user, ...this.entries]
+        this.update()
+        this.save()
+     } catch(error){
+      alert(error.message)
+    }
+  }
+  
   delete(user) {
     //higher-order functions (map, filter, find, reduce)
     const filteredEntries = this.entries.filter(
@@ -35,6 +50,7 @@ export class Favorites {
 
     this.entries = filteredEntries;
     this.update();
+    this.save()
   }
 }
 
@@ -42,15 +58,21 @@ export class Favorites {
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root);
-
     this.tbody = this.root.querySelector("table tbody");
-
     this.update();
+    this.onadd();
+  }
+
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+      this.add(value)
+    }
   }
 
   update() {
     this.removeAllTr();
-
     this.entries.forEach((user) => {
       const row = this.createRow();
 
@@ -58,6 +80,7 @@ export class FavoritesView extends Favorites {
         ".user img"
       ).src = `https://github.com/${user.login}.png`;
       row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
       row.querySelector(".user p").textContent = user.name;
       row.querySelector(".user span").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
---
+++ js/GithubUser.js
@@ -0,0 +1,15 @@
+//Conexão com Api do git para salvar JSON em localStorage
+export class GitHubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`;
+
+    return fetch(endpoint)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }));
+  }
+}
